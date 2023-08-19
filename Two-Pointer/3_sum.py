90427c6c3c0860b3b3b1cce9e8cff1f3f06b4049
pujan tamang <pujantamang92@gmail.com>
2023-08-19 14:08:10 +0100

    added 3-sum.py

---
+++ Two-Pointer/3_sum.py
@@ -0,0 +1,36 @@
+# 0(n*2)
+
+# first we sort the array in ascending order
+# then we choose the first value and perform 2sum on l+1 index
+# since, we are operating on current index
+# to neglect duplicate make sure current value is not equal to previous value
+# both on 2sum and outer current value
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            # Skip positive integers
+            if a > 0:
+                break
+
+            if i > 0 and a == nums[i - 1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+
+        return res

b47e252689a2cf850170ceb7974e9be018f97e77
pujan tamang <pujantamang92@gmail.com>
2023-08-24 21:07:44 +0100

    explanation of sliding-window-maximum.py

--- Sliding-Window/sliding_window_maximum.py
+++ Sliding-Window/sliding_window_maximum.py
@@ -1,5 +1,18 @@
 import collections
 
+# O(n)
+# initialize list to represent list of maximum number in sliding window
+# we initialize a queue for setting the max in sliding window at partcular iteration
+# initialize the left and right pointer at 0 since we are using while
+# while right pointer is less than length of list (setting the boundary)
+# then we need to add the current index to queue
+# if there are value in queue and queue value is less than current, we pop the queue
+# then insert current value
+# if our window is out of bounds remove the leftmost value (end of cur iter)
+# if our loop is equal to sliding wiwndow size 'k' then append the max value(leftmost) and increase
+# left pointer by 1
+# we need to increase r by 1 in every iteration
+
 
 class Solution:
     def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:

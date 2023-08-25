1cd8c9eb7d1ebf74f95034db298d1fac938e53ff
pujan tamang <pujantamang92@gmail.com>
2023-08-25 13:28:28 +0100

    added min-stack.py

---
+++ Stack/min_stack.py
@@ -0,0 +1,32 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+["MinStack", "push", "push", "push", "getMin", "pop", "top", "getMin"]
+[[], [-2], [0], [-3], [], [], [], []]
+
+minStack = MinStack()
+print(minStack.push(-2))
+print(minStack.push(0))
+print(minStack.push(-3))
+print(minStack.getMin())
+print(minStack.pop())
+print(minStack.top())
+print(minStack.getMin())

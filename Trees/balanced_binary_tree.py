dcabaf5954c8f72a64d627e0195b1e3e8df61f35
pujan tamang <pujantamang92@gmail.com>
2023-09-08 08:59:35 +0100

    balanced-binary-tree.py

---
+++ Trees/balanced_binary_tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# we initialize that first parameter is bool and second is the max height
+# left and right will be return value
+# to know its balanced tree both left and right first param needs to be true
+# difference in their tree should be less than 1
+# return balanced and height
+
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return [True, 0]
+
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]

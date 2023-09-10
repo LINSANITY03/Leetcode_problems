6649a2892ee65f5bd3c35f06b049de97e2167e96
pujan tamang <pujantamang92@gmail.com>
2023-09-10 14:12:15 +0100

    added subtree-of-another-tree.py

---
+++ Trees/subtree_of_another_tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# dfs approach
+# check if the treenode of t is none then we can return true
+# if the both node are identical return true
+# else start from left node then right node until none
+
+
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not t:
+            return True
+        if not s:
+            return False
+
+        if self.sameTree(s, t):
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+
+    def sameTree(self, s, t):
+        if not s and not t:
+            return True
+        if s and t and s.val == t.val:
+            return self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
+        return False

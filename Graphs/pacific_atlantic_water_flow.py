9ccb3e71d29b907f35a09124f7a268ac5406625a
pujan tamang <pujantamang92@gmail.com>
2023-10-06 20:39:24 +0100

    added pacific-atlantic-water-flow.py

--- Graphs/pacific_atlantic_water_flow.py
+++ Graphs/pacific_atlantic_water_flow.py
@@ -46,3 +46,8 @@ class Solution:
                 if (r, c) in pac and (r, c) in atl:
                     res.append([r, c])
         return res
+
+
+heights = [[1, 2, 2, 3, 5], [3, 2, 3, 4, 4], [
+    2, 4, 5, 3, 1], [6, 7, 1, 4, 5], [5, 1, 1, 2, 4]]
+print(Solution().pacificAtlantic(heights))
---
+++ Graphs/surrounded_regions.py
@@ -0,0 +1,41 @@
+# we need to find the regions that are not surrounded by water
+# logic behind this is to find the bounded region and delete it, leaving unbounded on board
+# so we know that "O" region in the outer layer are always unbounded
+# find all the "O" region and any other connected to this region then convert "O" to "T"
+# after this convert all the remaining "O" to "X" , as the survivor "O" are all bounded
+
+class Solution:
+    def solve(self, board: list[list[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+
+        def capture(r, c):
+            if r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != "O":
+                return
+            board[r][c] = "T"
+            capture(r + 1, c)
+            capture(r - 1, c)
+            capture(r, c + 1)
+            capture(r, c - 1)
+
+        # 1. (DFS) Capture unsurrounded regions (O -> T)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O" and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
+                    capture(r, c)
+
+        # 2. Capture surrounded regions (O -> X)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+
+        # 3. Uncapture unsurrounded regions (T -> O)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "T":
+                    board[r][c] = "O"
+
+
+board = [["X", "X", "X", "X"], ["X", "O", "O", "X"],
+         ["X", "X", "O", "X"], ["X", "O", "X", "X"]]
+print(Solution().solve(board))
